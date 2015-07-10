762f880dfe3d9dcd6068f4dc8c2193364d31344c
Yvan Janssens <ik@yvanj.me>
2015-07-10 16:41:14 +0100

    Added the bar app to the url list as the main app>

--- bar/views.py
+++ bar/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from django.template import RequestContext, loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bar index.")
+    products = Product.objects.order_by('name')
+    output = ', '.join([p.name for p in products])
+    template = loader.get_template('bar/index.html')
+    context = RequestContext(request, {
+        'products' : products
+    })
+
+    return HttpResponse(template.render(context))
