a6a3447b87e9c67bd8023aaecf588f46dd0db402
Yvan Janssens <ik@yvanj.me>
2015-07-10 15:33:44 +0100

    Defined the "Sale" and 'Product" models.

--- bar/models.py
+++ bar/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.contrib.auth import models as user_models
 
 # Create your models here.
+
+class Product(models.Model):
+    PRODUCT_TYPES = (
+        ('S', 'Snack'),
+        ('D', 'Drink'),
+        ('A', 'Alcoholic Beverage')
+    )
+
+    name = models.TextField()
+    description = models.TextField()
+    product_type = models.CharField(max_length=1, choices=PRODUCT_TYPES)
+    product_price = models.DecimalField(decimal_places=2, max_digits=6)
+
+
+class Sale(models.Model):
+    product_sold = models.ForeignKey(Product)
+    datetime_sold = models.DateTimeField(auto_now=True)
+    customer = models.ForeignKey(user_models.User)
 
